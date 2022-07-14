2dd524ec3956102e68130f10d8cceb401abf32b8
IvanChernenko <ivanchernenko92@gmail.com>
2022-07-14 20:11:33 +0300

    Update loadMore.js

--- src/loadMore.js
+++ src/loadMore.js
@@ -10,8 +10,9 @@ refs.loadMoreBtn.addEventListener('click', loadMore);
 
 function loadMore(e) {
     totalHits = fetchData.totalHits;
-    const numberOfPages = Math.round(totalHits / 40);
-    
+    const numberOfPages = Math.ceil(totalHits / 40);
+    console.log(page)
+    console.log(numberOfPages)
     if (page <= numberOfPages) {
         fetchPictures(e);
         console.log(page);
